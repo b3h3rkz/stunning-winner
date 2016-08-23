d901d04377d48466d79ad196c61801f81dcd2fa7
bernard <barjebernard@gmail.com>
2016-08-23 10:51:25 -0400

    added static files

--- Excels/Excels/settings.py
+++ Excels/Excels/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'Excels.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR,'static/')
