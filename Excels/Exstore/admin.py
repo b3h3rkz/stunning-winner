88402eaee30b828116e075833071779922019519
bernard <barjebernard@gmail.com>
2016-08-23 06:35:20 -0400

    created models for data collection

--- Excels/Exstore/admin.py
+++ Excels/Exstore/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import UserData
 
-# Register your models here.
+class UserDataAdmin(admin.ModelAdmin):
+    model = UserData
+    list_display = ['first_name', 'last_name', 'age', 'gender', 'address']
+
+
+admin.site.register(UserData, UserDataAdmin)
--- Excels/Exstore/models.py
+++ Excels/Exstore/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class UserData(models.Model):
+    GENDER_CHOICES = (
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+    )
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    gender = models.CharField(default='Male', choices=GENDER_CHOICES, max_length=6)
+    address = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = 'User Data'
+
+    def __str__(self):
+        return self.fullname()
+
+    def fullname(self):
+        return self.first_name + self.last_name
